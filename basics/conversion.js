20320fb0083d1870725f7ea55d495267d287636f
Saiprasad100 <121687380+Saiprasad100@users.noreply.github.com>
2025-02-13 07:18:07 +0000

    type conversion demo

---
+++ basics/conversion.js
@@ -0,0 +1,21 @@
+let score = "Sai"
+
+//console.log(typeof score)
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);//number ===> because of score converted to number
+//console.log(valueInNumber) // NaN------> because Sai is "Not a Number"
+
+//33  ---> 33
+//"33as" ---> Nan
+//true ===>1    false ===>0
+
+let isLoggedIn = "prasad"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1====> true  0====>false
+//"" ===> false
+//"Saiprasd"====>true
