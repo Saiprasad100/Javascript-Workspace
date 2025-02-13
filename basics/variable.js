f008c5c3d91fabe2d63fbe59c02fbb7112f5b2ad
Saiprasad100 <121687380+Saiprasad100@users.noreply.github.com>
2025-02-13 06:00:32 +0000

    variable demo example

---
+++ basics/variable.js
@@ -0,0 +1,19 @@
+const accountId = 124521
+
+let accountEmail = "sai@gmail.com"
+
+var accountPassword = "Das@123"
+
+accountCity="Bengaluru"
+
+//accountId =454545454//Not allowed to re-initialize the const variable 
+
+accountEmail="prasad@123"
+accountPassword = 456789963
+accountCity="Bubaneswer"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+not recomonded to use var ,it is used in function and block level
+*/
