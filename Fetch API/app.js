6b8a5ead8c19057b414ca2b2b8eb98ace4ac9a99
Saiprasad100 <121687380+Saiprasad100@users.noreply.github.com>
2025-03-10 07:59:50 +0000

    final commit

---
+++ Fetch API/app.js
@@ -0,0 +1,32 @@
+/*const URL = "https://api.thecatapi.com/v1/images/search";
+
+
+let getFacts = async() => {
+    console.log("Getting Data...");
+    
+    let response = await fetch(URL);
+    console.log(response);
+} 
+*/
+
+/*const URL = "https://api.thecatapi.com/v1/images/search";
+
+let getFacts = async () => {
+    console.log("Getting Data...");
+    
+    let response = await fetch(URL); // Fetch data from API
+    console.log(response);
+    
+    let data = await response.json(); // Convert response to JSON
+    console.log(data); // Print actual data
+    console.log(response.status);
+    
+    
+
+};*/
+
+//getFacts(); // Call the function
+
+
+
+
